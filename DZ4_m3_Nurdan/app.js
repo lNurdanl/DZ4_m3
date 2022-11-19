c42966efd28262767ac0b3781f0def97655be6a4
lNurdanl <115089718+lNurdanl@users.noreply.github.com>
2022-11-19 18:49:42 +0600

    Add files via upload

---
+++ DZ4_m3_Nurdan/app.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+    const request = new XMLHttpRequest(); //создание запроса
+    request.open("GET", "me.json")
+    request.setRequestHeader('Content-type', "application/json")
+    request.send();
+    request.addEventListener('load', () => {
+        console.log(JSON.parse(request.response));
+        const data = JSON.parse(request.response)
+        document.querySelector(".name1").innerHTML = data.name1
+        document.querySelector(".surname").innerHTML = data.surname
+        document.querySelector(".age").innerHTML = data.age
+        document.querySelector(".hobby").innerHTML = data.hobby
+        document.querySelector(".school").innerHTML = data.school
+    })
+});
